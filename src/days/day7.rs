28fe8b4f732bd1ac001620a430762d3d30d1652b
Orwa Shams <orwa.s.shams@gmail.com>
2023-07-29 17:38:52 +0300

    day7 progress

--- src/days/day7.rs
+++ src/days/day7.rs
@@ -27,16 +27,14 @@ pub fn solve() {
     let input = fs::read_to_string("input.txt").unwrap();
 
     let mut dir_stack = vec!["/"];
-    let mut current_dir = Node::Dir(Dir {
-        name: "/",
-        size: 0,
-        nodes: vec![],
-    });
+    let mut cwd = "/";
+    let mut listed_dirs: Vec<&str> = vec![];
 
     for line in input.lines() {
         let line = line.trim();
 
         if !line.starts_with("$") {
+            println!("Invalid line: {line}");
             continue;
         }
 
@@ -46,20 +44,41 @@ pub fn solve() {
             match command {
                 Command::Cd => match arg {
                     "/" => {
-                        current_dir = Node::Dir(Dir {
-                            name: "/",
-                            size: 0,
-                            nodes: vec![],
-                        })
+                        cwd = "/";
+                        dir_stack.clear();
+                        dir_stack.push("/");
+                        println!("called cd /, before pop, dir_stack = {:?}", dir_stack);
                     }
                     ".." => {
+                        println!("called .., before pop, dir_stack = {:?}", dir_stack);
                         dir_stack.pop();
+                        println!("called .., after pop, dir_stack = {:?}", dir_stack);
                     }
                     _ => {
+                        println!(
+                            "called cd on {arg}, before push, dir_stack = {:?}",
+                            dir_stack
+                        );
                         dir_stack.push(arg);
+                        println!(
+                            "called cd on {arg}, after push, dir_stack = {:?}",
+                            dir_stack
+                        );
+
+                        println!("current_dir before change = {:?}", cwd);
+                        cwd = dir_stack.last().unwrap();
+                        println!("current_dir after change = {:?}", cwd);
                     }
                 },
-                Command::Ls => {}
+                Command::Ls => {
+                    println!(
+                        "bla bla: {:?}",
+                        input.lines().take(4).collect::<Vec<&str>>()
+                    );
+                    println!("lised ls, arg = {:?}", listed_dirs);
+                    println!("called ls,current_dir = {:?}", cwd);
+                    listed_dirs.clear();
+                }
             }
         });
     }
