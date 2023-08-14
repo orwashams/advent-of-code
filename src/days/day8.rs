0f01fdef39af877920f0533d06ed564e34dcc449
Orwa Shams <orwa.s.shams@gmail.com>
2023-08-14 21:47:39 +0300

    read input to a 2d vec

--- src/days/day8.rs
+++ src/days/day8.rs
@@ -1,1 +1,19 @@
-pub fn solve() {}
+use std::fs;
+
+pub fn solve() {
+    let input = fs::read_to_string("input.txt").unwrap();
+
+    let grid: Vec<Vec<usize>> = input
+        .lines()
+        .map(|line| {
+            line.chars()
+                .map(|c| c.to_digit(10).unwrap() as usize)
+                .collect()
+        })
+        .collect();
+    println!("{:?}", grid[0]);
+
+    let width = input.lines().next().unwrap().trim().len();
+    let height = input.lines().count();
+    println!("width: {}, height: {}", width, height);
+}
